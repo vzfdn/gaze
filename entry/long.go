58044a7ecde893cc30cdab0627d7dd5814f0ad73
vzfdn <arvinvazife@protonmail.com>
2025-02-02 17:29:49 +0330

    Refactor Long function to align columns dynamically based on max width for better formatting

--- entry/long.go
+++ entry/long.go
@@ -3,22 +3,50 @@ package entry
 import (
 	"fmt"
 	"strings"
+	"unicode/utf8"
 )
 
-func Long(displayEntries []Entry) string {
+// Long formats and returns a detailed table-like string representation of the given entries.
+func Long(entries []Entry) string {
 	var sb strings.Builder
-	line := fmt.Sprintf("%d File,%s\n", len(displayEntries), TotalSize(displayEntries))
+
+	// Calculate maximum widths for each column
+	var maxUserLen, maxGroupLen, maxSizeLen, maxTimeLen int
+	for _, e := range entries {
+		user, group := e.UserAndGroup()
+		sizeStr := HumanReadableSize(e.Size()) 
+		timeStr := e.Time()                     
+
+		if utf8.RuneCountInString(user) > maxUserLen {
+			maxUserLen = utf8.RuneCountInString(user)
+		}
+		if utf8.RuneCountInString(group) > maxGroupLen {
+			maxGroupLen = utf8.RuneCountInString(group)
+		}
+		if utf8.RuneCountInString(sizeStr) > maxSizeLen {
+			maxSizeLen = utf8.RuneCountInString(sizeStr)
+		}
+		if utf8.RuneCountInString(timeStr) > maxTimeLen {
+			maxTimeLen = utf8.RuneCountInString(timeStr)
+		}
+	}
+
+	// Header line (total files and size)
+	line := fmt.Sprintf("%d File, %s\n", len(entries), TotalSize(entries))
 	sb.WriteString(line)
-	const pad = 2
-	for _, e := range displayEntries {
-		line = fmt.Sprintf("%s%*s%s%*s%s%*s%s%*s%s\n",
-			e.Permission(), pad, "",
-			e.UserAndGroup(), pad, "",
-			e.Time(), pad, "",
-			HumanReadableSize((e.Size())), pad, "",
+
+	for _, e := range entries {
+		user, group := e.UserAndGroup()
+		line = fmt.Sprintf("%s  %-*s  %-*s  %-*s  %-*s  %s\n",
+			e.Permission(),
+			maxUserLen, user,     
+			maxGroupLen, group,   
+			maxTimeLen, e.Time(),  
+			maxSizeLen, HumanReadableSize(e.Size()),  
 			e.Name(),
 		)
 		sb.WriteString(line)
 	}
+
 	return sb.String()
 }
